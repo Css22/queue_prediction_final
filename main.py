de52709a18feb955ceba95c83f6ffd32c93d69cc
Css22 <2585783520@qq.com>
2022-05-27 15:10:00 +0800

    GBDT 回归预测
    需要调参

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from model.sklearn_logistic_regression import SklearnLogisticRegressionModel
 from model.DecisionTreeClassifier import DecisionTreeClassifier
 from model.DecisionTreeRegressor import DecisionTreeRegressor
 from model.GradientBoostingClassifier import GB_Classifier
+from model.GradientBoostingRegressor import GB_Regressor
 from preprocess.preprocess_theta import PreprocessorTheta
 from preprocess.Preprocessor_mira import PreprocessorMira
 from preprocess.preprocess_taiyi import PreprocessorTaiyi
@@ -12,15 +13,15 @@ from sample.class_labeling import Labeler
 
 if __name__ == '__main__':
     # preprocess
-    preprocessor = PreprocessorMira()
+    preprocessor = PreprocessorTaiyi()
     # raw_list = preprocessor.preprocess('data/local/mira/sorted.csv')
     # preprocessor.save(raw_list,'data/local/mira/RawSample_saved.txt')
     # raw_list = preprocessor.load('data/local/Taiyi/taiyi_raw_samples.txt')
-    # raw_list = preprocessor.load('data/local/theta/RawSample_saved.txt')
     raw_list = preprocessor.load('data/local/mira/RawSample_saved.txt')
+    # raw_list = preprocessor.load('data/local/mira/RawSample_saved.txt')
     # generate sample
     # sample_list = to_sample_list(raw_list)
-    # sample_save(sample_list, 'data/local/theta/sample_saved.txt')
+    # sample_save(sample_list, 'data/local/Taiyi/sample_saved.txt')
     sample_list = sample_load('data/local/mira/sample_saved.txt')
 
     # labelin
@@ -30,21 +31,14 @@ if __name__ == '__main__':
     sample_list = labeler.label_samples(sample_list)
 
     # # run model
-    # model = SklearnRegressionModel(sample_list, labeler, 7, 1, raw_list)
-    # model = RegressionModel(sample_list, labeler, 4, 1, raw_list)
-    model = GB_Classifier(sample_list, labeler, 7, 1, raw_list)
+    model = GB_Regressor(sample_list, labeler, 7, 1, raw_list)
+
     # 修改聚类，取消聚类，并以queue_name作为分类标准。
     # model.label_queue_name()
 
     model.create_dataset()
     model.train()
     model.test()
-    # AAEtemp, PPEtemp = model.test()
-    # AAE.append(AAEtemp)
-    # PPE.append(PPEtemp)
-    # print(AAE)
-    # print(PPE)
-    # model.save('linear_model.pkt')
 
     # # use model
     # print(model.predict(sample_list[0]))
